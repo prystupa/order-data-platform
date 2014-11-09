1ce14dd77b19cebc2788b8a8811714da5349daaf
prystupa <eugene.prystupa@gmail.com>
2014-11-08 19:25:58 -0500

    - partition aware key

---
+++ src/main/java/com/prystupa/EventID.java
@@ -0,0 +1,37 @@
+package com.prystupa;
+
+import com.hazelcast.core.PartitionAware;
+
+import java.io.Serializable;
+
+public class EventID implements PartitionAware<String>, Serializable {
+
+    private final String eventId;
+    private final String primeId;
+
+    public EventID(String eventId, String primeId) {
+        this.eventId = eventId;
+        this.primeId = primeId;
+    }
+
+    @Override
+    public String getPartitionKey() {
+        return primeId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EventID eventID = (EventID) o;
+
+        return eventId.equals(eventID.eventId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return eventId.hashCode();
+    }
+}
