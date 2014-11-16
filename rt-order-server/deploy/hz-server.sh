5ceb7750c3891e4dc3ee8a448cc3ac20d995b4c3
prystupa <eugene.prystupa@gmail.com>
2014-11-15 20:55:36 -0500

    setup release to docker - fix docker paths

--- rt-order-server/deploy/hz-server.sh
+++ rt-order-server/deploy/hz-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build https://github.com/prystupa/rt-order-reporting/releases/download/v0.5/hz-server.Dockerfile
+docker build https://github.com/prystupa/rt-order-reporting/releases/download/v0.7/hz-server.Dockerfile
