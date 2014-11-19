70a046c80a8dae180b06be8eb6fc0649f7164d18
Eugene Prystupa <eugene.prystupa@lab49.com>
2014-11-19 09:21:50 -0500

    no sudo for service startup

--- rt-order-server/deploy/setup.sh
+++ rt-order-server/deploy/setup.sh
@@ -2,6 +2,6 @@
 # Abort our setup if anything goes wrong.
 set -e
 
-sudo yum install -y -q java-1.7.0-openjdk-devel
+yum install -y -q java-1.7.0-openjdk-devel
 
 java -Daws.access-key=$AWS_ACCESS_KEY -Daws.secret-key=$AWS_SECRET_KEY -cp './*' com.prystupa.server.StoreApp
