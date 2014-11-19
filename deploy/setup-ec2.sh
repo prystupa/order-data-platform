b8e897de4c33a2a4c02884f9fa47a9faf2deb2c3
Eugene Prystupa <eugene.prystupa@lab49.com>
2014-11-19 07:26:56 -0500

    bootstrapping to Linux directly without docker

---
+++ deploy/setup-ec2.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Abort our setup if anything goes wrong.
+set -e
+
+# Our upstart script redirects all output of this script to /var/log/app-setup.log
+# We can debug the output looking for when the script started.
+echo "at=start-setup time=`date`"
+
+# We are making use of EC2's user data feature. When you provision a new
+# EC2 instance, you can supply arbitrary text as the user data. In the case
+# our setup script, we are assuming that the data in the user data store is
+# valid bash. Specifically we are expecting that it contain variable declerations
+# for $RELEASE_URL and $APP
+curl http://169.254.169.254/latest/user-data -o user-data
+while read -r line; do
+    export "$line"
+done < user-data
+
+# Download the code via RELEASE_URL - which is supplied by user-data. We
+# expect the tar ball to include a single directory in which the app's
+# code and 'setup.sh' are located.
+rm -rf app
+mkdir app
+curl -L $RELEASE_URL | tar xvz -C app
+
+cd app/
+
+./setup.sh
---
+++ rt-order-server/deploy/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Abort our setup if anything goes wrong.
+set -e
+
+yum install -y -q java-1.7.0-openjdk-devel
+
+java -Daws.access-key=$AWS_ACCESS_KEY -Daws.secret-key=$AWS_SECRET_KEY -cp './*' com.prystupa.server.StoreApp
