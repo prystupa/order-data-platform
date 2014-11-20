12de1e45c1c0afd9e703da4bccb5285412830321
Eugene Prystupa <eugene.prystupa@lab49.com>
2014-11-20 10:12:16 -0500

    fix typo in class name reference

--- rt-order-client/deploy/setup.sh
+++ rt-order-client/deploy/setup.sh
@@ -4,4 +4,4 @@ set -e
 
 yum install -y -q java-1.7.0-openjdk-devel
 
-java -Daws.access-key=$AWS_ACCESS_KEY -Daws.secret-key=$AWS_SECRET_KEY -cp './*' com.prystupa.client.SimulatedLoadApp
+java -Daws.access-key=$AWS_ACCESS_KEY -Daws.secret-key=$AWS_SECRET_KEY -cp './*' com.prystupa.client.SimulateLoadApp
