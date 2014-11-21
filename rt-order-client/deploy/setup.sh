f5b36056f6c8ebb4e8c306c560fa7b92d0943fde
Eugene Prystupa <eugene.prystupa@lab49.com>
2014-11-21 08:56:08 -0500

    add --quiet to wget

--- rt-order-client/deploy/setup.sh
+++ rt-order-client/deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 yum install -y -q wget
 
 pushd /opt
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz"
+wget --quiet --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz"
 tar xzf jdk-8u25-linux-x64.tar.gz
 
 cd /opt/jdk1.8.0_25/
