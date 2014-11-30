73716e4b2708d98f0445392694caf43b0b77d23b
prystupa <eugene.prystupa@gmail.com>
2014-11-29 21:38:44 -0500

    undo zone

--- ec2/deploy-clients.sh
+++ ec2/deploy-clients.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ec2-run-instances ami-5613813e --instance-type ${I-m3.medium} --availability-zone ${Z-us-east-1c} --group hazelcast-cluster \
+ec2-run-instances ami-5613813e --instance-type ${I-m3.medium} --group hazelcast-cluster \
     --instance-count ${N-2} --user-data-file user-data-client.txt \
     --key sybase-iq \
     | grep INSTANCE | cut -f 2 \
