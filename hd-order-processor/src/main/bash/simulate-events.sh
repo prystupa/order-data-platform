e8483321f49e386857daf480dd0f516f393ab920
eprystupa <eugene.prystupa@gmail.com>
2014-12-30 12:23:41 -0500

    shell script to run hadoop simulated event generation

---
+++ hd-order-processor/src/main/bash/simulate-events.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Abort our setup if anything goes wrong.
+set -e
+
+LIBJARS=lib/`ls -m lib | sed -e 's/, /,lib\//g' | tr -d '\n'`
+hadoop jar hd-order-processor-1.0-SNAPSHOT.jar com.prystupa.generate.OrderEventGeneratorApp \
+    -libjars=$LIBJARS  \
+    -D event-count=${N:-1000000} \
+    -D verbose=${VERBOSE:-false} \
+    ${OUT:-output}
--- hd-order-processor/src/main/java/com/prystupa/generate/OrderEventGeneratorApp.java
+++ hd-order-processor/src/main/java/com/prystupa/generate/OrderEventGeneratorApp.java
@@ -4,6 +4,7 @@ import com.prystupa.generate.mapreduce.OrderEventByOmsPartitioner;
 import com.prystupa.generate.mapreduce.OrderEventGeneratorMapper;
 import com.prystupa.generate.mapreduce.OrderEventWritable;
 import com.prystupa.generate.mapreduce.SimulatorParametersInputFormat;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
@@ -11,9 +12,12 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OrderEventGeneratorApp extends Configured implements Tool {
 
+    private static Logger logger = LoggerFactory.getLogger(OrderEventGeneratorApp.class);
     private static final int OMS_TOTAL = 40;
 
     public static void main(String[] args) throws Exception {
@@ -30,6 +34,7 @@ public class OrderEventGeneratorApp extends Configured implements Tool {
             return -1;
         }
 
+        logger.info("Starting the job");
         Job job = Job.getInstance(getConf());
         job.setJarByClass(getClass());
         job.setInputFormatClass(SimulatorParametersInputFormat.class);
@@ -40,6 +45,10 @@ public class OrderEventGeneratorApp extends Configured implements Tool {
         job.setNumReduceTasks(OMS_TOTAL);
         FileOutputFormat.setOutputPath(job, new Path(args[0]));
 
-        return job.waitForCompletion(false) ? 0 : 1;
+        Configuration configuration = job.getConfiguration();
+        boolean succeeded = job.waitForCompletion(configuration.getBoolean("verbose", false));
+        logger.info("Completed the job, succeeded: {}", succeeded);
+
+        return succeeded ? 0 : 1;
     }
 }
