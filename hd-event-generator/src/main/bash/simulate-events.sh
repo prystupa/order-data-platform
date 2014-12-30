46a447f460bedca811d53071b3744bc029565911
eprystupa <eugene.prystupa@gmail.com>
2014-12-30 17:10:11 -0500

    refactoring

--- hd-event-generator/src/main/bash/simulate-events.sh
+++ hd-event-generator/src/main/bash/simulate-events.sh
@@ -3,7 +3,7 @@
 set -e
 
 LIBJARS=lib/`ls -m lib | sed -e 's/, /,lib\//g' | tr -d '\n'`
-hadoop jar hd-order-processor-1.0-SNAPSHOT.jar com.prystupa.generate.OrderEventGeneratorApp \
+hadoop jar hd-event-generator-1.0-SNAPSHOT.jar com.prystupa.generate.OrderEventGeneratorApp \
     -libjars=$LIBJARS  \
     -D event-count=${N:-1000000} \
     -D verbose=${VERBOSE:-false} \
