2f844b07c79ef930ffacd4d751c008fbe1b0b973
eprystupa <eugene.prystupa@gmail.com>
2014-12-30 15:05:02 -0500

    map reduce to count events

---
+++ hd-order-processor/src/main/java/com/prystupa/generate/OrderEventCountApp.java
@@ -0,0 +1,49 @@
+package com.prystupa.generate;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.TaskCounter;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsBinaryInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderEventCountApp extends Configured implements Tool {
+
+    private static Logger logger = LoggerFactory.getLogger(OrderEventCountApp.class);
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new OrderEventCountApp(), args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.printf("Usage: %s: [generic options] <input>\n", getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        logger.info("Starting the job");
+        Job job = Job.getInstance(getConf());
+        job.setJarByClass(getClass());
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        job.setInputFormatClass(SequenceFileAsBinaryInputFormat.class);
+        job.setOutputFormatClass(NullOutputFormat.class);
+        job.setNumReduceTasks(0);
+
+        boolean succeeded = job.waitForCompletion(getConf().getBoolean("verbose", false));
+        if (succeeded) {
+            System.out.printf("Found %,d events\n", job.getCounters().findCounter(TaskCounter.MAP_INPUT_RECORDS).getValue());
+        } else {
+            System.err.println("The job failed");
+        }
+
+        return succeeded ? 0 : 1;
+    }
+}
