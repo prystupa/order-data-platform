ba81918cc469932c011f1caaa14ef7dd27f2b6e7
eprystupa <eugene.prystupa@gmail.com>
2014-12-31 16:02:46 -0500

    fixed simulate and counts scripts

--- hd-event-generator/src/main/bash/count-events.sh
+++ hd-event-generator/src/main/bash/count-events.sh
@@ -6,8 +6,7 @@ LIBJARS=lib/`ls -m lib | tr -d '\n' | tr -d ' ' | sed -e 's/,/,lib\//g'`
 HADOOP_CLASSPATH=lib/`ls -m lib | tr -d '\n' | tr -d ' ' | sed -e 's/,/:lib\//g'`
 
 export HADOOP_CLASSPATH
-hadoop jar hd-event-generator-1.0-SNAPSHOT.jar com.prystupa.generate.OrderEventGeneratorApp \
+hadoop jar hd-event-generator-1.0-SNAPSHOT.jar com.prystupa.generate.OrderEventCountApp \
     -libjars=$LIBJARS  \
-    -D event-count=${N:-1000000} \
     -D verbose=${VERBOSE:-false} \
-    ${OUT:-events/`date "+%Y-%m-%d"`}
+    ${IN:-events/`date "+%Y-%m-%d"`}
